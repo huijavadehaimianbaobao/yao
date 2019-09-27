3115a53ad5c12e1713ffa58be7cae0e976eb4df7
张旭 <you@example.com>
2019-09-28 00:44:55 +0800

    2%
    2%

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,21 +5,34 @@ const app = getApp()
 Page({
   data: {
     current: 'index',
-    active: 0,
+    active: 1,
     sex: '1',
-    marriage:'1'
+    marriage:'1',
+    sickness:'1',
   },
 
   sexonChange(event) {
-    console.log(event)
     this.setData({
       sex: event.detail
     });
   },
   marriageonChange(event) {
-    console.log(event)
     this.setData({
       marriage: event.detail
     });
+  },
+  sicknessonChange(event) {
+    this.setData({
+      sickness: event.detail
+    });
+  },
+  saveBase:function(){
+    var that=this
+    console.log("保存信息")
+    var active = that.data.active
+      that.setData({
+        active:1
+    })
+   
   }
 })
